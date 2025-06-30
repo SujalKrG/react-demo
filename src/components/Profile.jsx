db545bf89c882e002347f439090f01e48d2c5893
sujalkrg <sujalkrghosh@gmail.com>
2025-06-30 14:11:31 +0530

    A short profile componenet to learn about useState

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,15 +1,51 @@
 import React, { useState } from "react";
 
 const Profile = () => {
+  const [profile, setProfile] = useState({
+    name: "",
+    age: "",
+  });
 
-    const [profile, setProfile] = useState({
-        name: '',
-        age: ''
-    });
-  return <div>
-    <h1>User Profile</h1>
-    <div></div>
-  </div>;
+  const handleChange = (e) => {
+    const {name, value} = e.target;
+
+    setProfile((prevProfile) => ({
+        ...prevProfile,
+        [name] : value,
+    }))
+  };
+  return (
+    <div>
+      <h1>User Profile</h1>
+      <div>
+        <label>
+          Name:
+          <input
+            type="text"
+            name="name"
+            value={profile.name}
+            onChange={handleChange}
+          />
+        </label>
+      </div>
+      <div>
+        <label>
+          Age:
+          <input
+            type="number"
+            name="age"
+            value={profile.age}
+            onChange={handleChange}
+          />
+        </label>
+      </div>
+      <div>
+        <h3>Profile Information</h3>
+        <p>Name: {profile.name}</p>
+        <p>Age: {profile.age}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
