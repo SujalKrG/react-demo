9b83a40da959e10273c15bee1204699fb6c4138c
sujalkrg <sujalkrghosh@gmail.com>
2025-07-04 13:09:33 +0530

    perfectly used useReducer to make a the counter component

--- src/components/UserReducerCounter.jsx
+++ src/components/UserReducerCounter.jsx
@@ -1,22 +1,39 @@
 import React, { useReducer } from "react";
 
 const UserReducerCounter = () => {
+  const initialState = { count: 0 };
   const reducer = (state, action) => {
-    if (action.type === "Increment") {
-      return state + 1;
-    }
-    if (action.type === "Decrement") {
-      return state - 1;
+    // if (action.type === "Increment") {
+    //   return state + 1;
+    // }
+    // if (action.type === "Decrement") {
+    //   return state - 1;
+    // }
+    // if (action.type === "Reset") {
+    //   return (state = 0);
+    // }
+
+    switch (action.type) {
+      case "Increment":
+        return { ...state, count: state.count + 1 };
+      case "Decrement":
+        return { ...state, count: state.count - 1 };
+      case "Reset":
+        return { count: 0 };
+
+      default:
+        break;
     }
   };
-  const [count, dispatch] = useReducer(reducer, 0);
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   return (
     <div>
       <h1>Counter using useReducer hook</h1>
-      <h1>{count}</h1>
+      <h1>{state.count}</h1>
       <button onClick={() => dispatch({ type: "Increment" })}>+</button>
       <button onClick={() => dispatch({ type: "Decrement" })}>-</button>
+      <button onClick={() => dispatch({ type: "Reset" })}>RESET</button>
     </div>
   );
 };
